b39fcaa5c7a83e7713da826b958747a098772efc
Sarah Wolff <sarah@serabi.dev>
2026-08-31 08:39:55 -0400

    chore(awesomux): bump cask to 0.15.0 (#16)
    * chore(awesomux): bump cask to 0.15.0
    
    * style(cask): reorder auto-updates stanza
    
    ---------
    
    Co-authored-by: awesoMux Release <noreply@interactivebuffoonery.com>

--- Casks/awesomux.rb
+++ Casks/awesomux.rb
@@ -1,6 +1,6 @@
 cask "awesomux" do
-  version "0.14.0"
-  sha256 "4dc07d597c6b175a36cc04d3ee534c7ad5511726d441bbbf634fa49f76761dea"
+  version "0.15.0"
+  sha256 "b2394935c6a43e26a5528a5f1833d4e46bd04cc1ae2045ab6fb2d6bf43a9402f"
 
   url "https://github.com/Interactive-Buffoonery/awesomux/releases/download/v#{version}/awesoMux-#{version}.dmg"
   name "awesoMux"
@@ -12,6 +12,7 @@ cask "awesomux" do
     strategy :github_latest
   end
 
+  auto_updates true
   depends_on macos: :sequoia
   depends_on arch: :arm64
 
